7e9a43e7ead3809d0358ea629e674c2b3441af20
John Kim <johnjohnskim@gmail.com>
2014-08-31 22:55:50 -0400

    More refactoring

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
-// SETUP
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var api = require('./routes/api');
+
 var app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-var port = process.env.PORT || 8080;
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-var api = require('./api');
-
-// RUN
 app.use('/api', api);
-app.listen(port);
+
+// error handlers
+
+// 404
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.end('Status: ' + err.status + '\n' + err.stack);
+  });
+}
+
+// production error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.end(err.message);
+});
+
+// module.exports = app;
+
+app.set('port', process.env.PORT || 8080);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var sql = require('./sql');
+var sql = require('../db/sql');
 
 var router = express.Router();
 
 router.use(function(req, res, next) {
-  console.log('Something is happening.');
+  console.log('api request made');
   next();
-});
-
-router.get('/', function(req, res) {
-  res.end('api up in here');
 });
 
 // PLAYERS
@@ -47,7 +43,39 @@ router.route('/players/:pid')
     });
   })
 
-// STAGES + CHARACTERS
+router.route('/fights')
+  .get(function(req, res) {
+    sql.getRows("SELECT * FROM fights", null, function(err, rows) {
+      res.json(rows);
+    });
+  })
+  .post(function(req, res) {
+    var name = req.body.name;
+    if (!name) {
+      return res.end('Need a name');
+    }
+    sql.insert("INSERT INTO u_fights (name) VALUES ($1)", [req.body.name], function(err, id) {
+      res.json(id);
+    });
+  })
+
+router.route('/fights/:fid')
+  .get(function(req, res) {
+    sql.getRows("SELECT * FROM fights WHERE id=$1", [req.params.fid], function(err, rows) {
+      res.json(rows[0]);
+    });
+  })
+  .put(function(req, res) {
+    var name = req.body.name;
+    if (!name) {
+      return res.end('Need a name');
+    }
+    sql.query("UPDATE players SET name=$1 WHERE id=$2", [req.body.name, req.params.pid], function(err, id) {
+      res.end('ok');
+    });
+  })
+
+// REST
 var views = ['stages', 'stagewins', 
   'characters', 'characterwins', 'charactermeta', 'charactervs',
   'playermeta', 'playervs'];
